f55265b9e45f3044e8a9518e942d66fb7406ef37
Error404 <apupaul35@gmail.com>
2025-06-06 09:33:05 +0600

    Intregate the GET API for get the books from server

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 app.use(cors());
-app.use(express.json()); 
-
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rbn9qen.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -17,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 // BookNest
 // qnpsgpNpetBKL2So
@@ -26,9 +25,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const booksCollection = client.db("bookNest").collection("books");
+
+    // books api
+
+    app.get('/books', async (req, res) => {
+      const cursor = booksCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -36,12 +50,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
   res.send("Book is incoming on server.");
 });
- 
+
 app.listen(port, () => {
   console.log(`Book is incoming on port ${port}`);
 });
